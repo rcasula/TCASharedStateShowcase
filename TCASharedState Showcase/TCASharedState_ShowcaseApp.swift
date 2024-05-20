269ad652217aa8ce594d9a18c27e7d3e3cb23a7c
Roberto Casula <roberto@casula.dev>
2024-05-20 09:30:32 +0200

    Initial Commit

---
+++ TCASharedState Showcase/TCASharedState_ShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASharedState_ShowcaseApp.swift
+//  TCASharedState Showcase
+//
+//  Created by Roberto Casula on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TCASharedState_ShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
